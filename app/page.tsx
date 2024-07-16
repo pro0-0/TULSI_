c94437919b4e91bbc3df1186119b90037358fde0
Shivam <shivamchandak9@gmail.com>
2024-07-17 01:27:10 +0530

    carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "../components/hero";
+import { InfiniteMovingCards } from "@/components/infinite-moving-cards";
+import { SwipeCarousel } from "@/components/SwipeCarousel";
 
 export default function Home() {
   return (
     <div className="w-full items-center justify-center">
       <Hero />
+      {/* <InfiniteMovingCards
+        items={[
+          { imgSrc: "/buliding.png", companyName: "Company 1" },
+          { imgSrc: "/logo.png", companyName: "Company 2" },
+          { imgSrc: "/windows.jpeg", companyName: "Company 3" },
+        ]}
+        direction="left"
+        speed="fast"
+        pauseOnHover={true}
+        className="additional-css-class"
+      /> */}
+      <SwipeCarousel />
     </div>
   );
 }
