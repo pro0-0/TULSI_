a200b39b5fd2e8dc30c96bf67a3f9cb996ff3251
Shivam <shivamchandak9@gmail.com>
2024-07-17 01:10:44 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Navbar mode="light" />
+      <body className={inter.className}>
+        <main className="relative overflow-hidden">{children}</main>
+      </body>
     </html>
   );
 }
